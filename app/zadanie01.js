52f47f889130495e8b8a2380c468d5b86bd4c130
maciej.menakiewicz <maciej.menakiewicz@allegro.pl>
2018-04-18 16:21:01 +0200

    Sum 2 numbers as url params and simple app for setting and showing name

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,16 @@
 //Twój kod
+const express = require("express");
+
+const app = express();
+
+
+app.get('/:n1/:n2', (req, res) => {
+   const n1 = parseInt(req.params.n1);
+   const n2 = parseInt(req.params.n2);
+   const sum = n1 + n2;
+   res.send("Suma: " + sum);
+});
+
+app.listen(3000, ()=> {
+   console.log('server on localhost:3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,25 @@
 //Twój kod
+
+const express = require("express");
+
+const app = express();
+
+let name = "";
+
+app.get('/name/set/:imie', (req, res) => {
+    name = req.params.imie;
+    res.send("name set");
+});
+
+app.get('/name/show', (req, res) => {
+    res.send("name is: " + name);
+});
+
+app.get('/name/check', (req, res) => {
+    const message = (name.length > 0) ? "imie ustawione" : "imie nieustawione";
+    res.send(message);
+});
+
+app.listen(3000, () => {
+   console.log("server on localhost:3000");
+});
